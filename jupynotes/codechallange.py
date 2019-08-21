c8ad98e1feea37132e12a1cc9e942299b3bbb108
subhrajit mohanty <techbiglover@gmail.com>
2019-08-21 07:45:17 +0530

    Create codechallange.py

---
+++ jupynotes/codechallange.py
@@ -0,0 +1,43 @@
+from itertools import combinations 
+main_tup = []
+even = []
+odd = []
+rm_tuple = []
+
+def rSubset(arr, r): 
+    return list(combinations(arr, r))
+
+def getData(num):
+    input_num = num[0]
+    check_num = num[1:]
+    
+    arr = range(1,input_num + 1) 
+    r = 2
+    main_tup.append(rSubset(arr, r))
+    
+    for i in range(1,9):
+        if(i%2 == 0):
+            even.append(i)
+        else:
+            odd.append(i)
+    
+    for i in range(len(odd)):
+        if i<=len(odd) - 2:
+            rm_tuple.append((odd[i],even[i+1]))
+        
+    for i in range(len(even)):
+        if i<=len(even) - 2:
+            rm_tuple.append((even[i],odd[i+1]))
+    
+    for i in main_tup[0]:
+        for j in check_num:
+            if(i[0] == j or i[1] == j):
+                rm_tuple.append(i)
+            if(i[1] > i[0] + r):
+                rm_tuple.append(i)                
+    
+                
+    return set(main_tup[0]) - set(rm_tuple)
+    
+    
+ getData([8, 1, 4, 8])
